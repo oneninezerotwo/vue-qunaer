ccb0dd4db1f67eb695a659c32a5bdfe75b6b36f9
Henvey <963778573@qq.com>
2019-06-10 23:49:09 +0800

    完整的去哪儿网项目

---
+++ my-projects/src/pages/city/city.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search
+     :cities="cities"
+     ></city-search>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter = letter
+      ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChande"
+      ></city-alphabet>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+export default {
+  name: 'city',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.headleGetCityInfoSucc)
+    },
+    headleGetCityInfoSucc (res) {
+      // console.log(res)
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChande (letter) {
+      this.letter = letter
+      // console.log(letter)
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ my-projects/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/home.vue'
+import City from '@/pages/city/city.vue'
+import Detail from '@/pages/detail/detail.vue'
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  }, {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
+  }],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
+})
